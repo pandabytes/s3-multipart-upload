9025afe18c0b8d174aceb0cb42f06c4bc0f40b5d
Huy Ngo <huybngo@yahoo.com>
2023-05-26 08:44:04 -0700

    Add base class for reader and writer

--- s3_multipart_upload/subcommands/io/uploaded_part.py
+++ s3_multipart_upload/subcommands/io/uploaded_part.py
@@ -16,7 +16,27 @@ class UploadedPart(BaseFrozenDataClass):
     if self.PartNumber <= 0:
       raise ValueError('PartNumber must be at least 1.')
 
-class UploadedPartFileWriter:
+class BaseUploadedPartFileReaderWriter:
+  """ Base class for uploaded part reader and writer. """
+  def __enter__(self):
+    self.open()
+    return self
+
+  def __exit__(
+    self,
+    _exc_type: type[BaseException] | None,
+    _exc_val: BaseException | None,
+    _exc_tb: types.TracebackType | None
+  ):
+    self.close()
+
+  def open(self):
+    raise NotImplemented()
+
+  def close(self):
+    raise NotImplemented()
+
+class UploadedPartFileWriter(BaseUploadedPartFileReaderWriter):
   def __init__(self, file_path: str, mode: Literal['w', 'a'], flush: bool = False) -> None:
     if mode not in {'w', 'a'}:
       raise ValueError('mode must be "w" or "a".')
@@ -26,16 +46,11 @@ class UploadedPartFileWriter:
     self._flush = flush
     self._writer = None
 
-  def __enter__(self):
+  def open(self):
     self._writer = jsonlines.open(self._file_path, mode=self._mode, flush=self._flush)
     return self
 
-  def __exit__(
-    self,
-    _exc_type: type[BaseException] | None,
-    _exc_val: BaseException | None,
-    _exc_tb: types.TracebackType | None
-  ):
+  def close(self):
     self._writer.close()
 
   def write(self, uploaded_part: UploadedPart):
@@ -44,21 +59,15 @@ class UploadedPartFileWriter:
 
     self._writer.write(uploaded_part.to_dict())
 
-class UploadedPartFileReader:
+class UploadedPartFileReader(BaseUploadedPartFileReaderWriter):
   def __init__(self, file_path: str) -> None:
     self._file_path = file_path
     self._reader = None
 
-  def __enter__(self):
+  def open(self):
     self._reader = jsonlines.open(self._file_path, mode='r')
-    return self
 
-  def __exit__(
-    self,
-    _exc_type: type[BaseException] | None,
-    _exc_val: BaseException | None,
-    _exc_tb: types.TracebackType | None
-  ):
+  def close(self):
     self._reader.close()
 
   def read(self):
