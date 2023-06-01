6aa4c31389f0a975893ce8aeffe3416b0b8b628c
Huy Ngo <huybngo@yahoo.com>
2023-05-31 19:48:25 -0700

    Refactor abort command

--- s3_multipart_upload/subcommands/abort/abort.py
+++ s3_multipart_upload/subcommands/abort/abort.py
@@ -3,13 +3,13 @@ from mypy_boto3_s3 import S3Client
 from s3_multipart_upload.logger import get_logger
 from s3_multipart_upload.io.multipart_meta import load_multipart_meta_file
 
-logger = get_logger('abort_logger')
+LOGGER = get_logger(__name__)
 
 def abort_multipart_upload(s3_client: S3Client, meta_file_path: str):
-  config = load_multipart_meta_file(meta_file_path)
-  if not config:
-    logger.error(f'Meta file {meta_file_path} not found or empty.')
+  multipart_meta = load_multipart_meta_file(meta_file_path)
+  if not multipart_meta:
+    LOGGER.error(f'Meta file {meta_file_path} not found or empty.')
     return
   
-  s3_client.abort_multipart_upload(Bucket=config.Bucket, Key=config.Key, UploadId=config.UploadId)
-  logger.info(f'Aborted multipart upload in {meta_file_path}.')
+  s3_client.abort_multipart_upload(Bucket=multipart_meta.Bucket, Key=multipart_meta.Key, UploadId=multipart_meta.UploadId)
+  LOGGER.info(f'Aborted multipart upload in {meta_file_path}.')
